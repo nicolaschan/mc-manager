a7e128bea5c1b0b398c88cbb6e2b6bb8ba9120c7
Nicolas Chan <nicolas@nicolaschan.com>
2017-10-05 18:53:56 -0700

    Added a comment

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,7 @@ fn start(server: String, verbose: bool) -> Result<(), Box<::std::error::Error>> 
     let server_dir_path = Path::new(&server);
     let screen_name = get_config(&server)?.screen;
 
+    // Start a new screen for the server
     let cmd = Command::new("screen")
         .args(&["-dmS", screen_name.as_str(), "./start-server.sh"])
         .current_dir(&server_dir_path)
